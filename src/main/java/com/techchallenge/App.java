7c16c2e805fe185556169a8cde3cf3ebfc02c2b1
Kanawade_D <devdattakwd@gmail.com>
2019-04-22 19:16:19 -0400

    initial commit

---
+++ src/main/java/com/techchallenge/App.java
@@ -0,0 +1,19 @@
+package com.techchallenge;
+
+/**
+ * #Problem Statement : #1. Count different combinations of 'x' consecutive cells in K x K grid #2. Calculate max product of 'x' consecutive cells in
+ * K x K grid
+ */
+public class App {
+
+  public long getTotalCombinations(int[][] grid, int adjucentElements) {
+    return 0L;
+  }
+
+  public long getProduct(int[][] grid, int length) {
+    return 0L;
+  }
+
+  public static void main(String[] args) {
+  }
+}
